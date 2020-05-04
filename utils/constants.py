1644f69e0b7d9096ced1322dc2eea76d361fca31
Wai Jean Koh <kwj_97@hotmail.com>
2020-05-04 21:42:23 +0100

    harden path

--- utils/constants.py
+++ utils/constants.py
@@ -1,7 +1,14 @@
+import os
+
+import git
+
 COL_HEADLINE = "news_headline"
 COL_ARTICLE = "news_article"
 COL_CATEGORY = "news_category"
 COL_DATE = "date"
 
-ROOT_DIR = "D:/Dev/Project/nlp_news/"
-LOG_CONFIG_PATH = "D:/Dev/Project/nlp_news/utils/logging.conf"
+repo = git.Repo(".", search_parent_directories=True)
+ROOT_DIR_PATH = repo.working_tree_dir
+
+LOG_CONFIG_PATH = os.path.join(ROOT_DIR_PATH, "utils/logging.conf")
+DATA_PATH = os.path.join(ROOT_DIR_PATH, "web_scraping/data")
