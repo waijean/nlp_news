46aff99c5aaee77ef45d992f4115d5ba863aaf1e
Wai Jean Koh <kwj_97@hotmail.com>
2020-05-04 11:28:11 +0100

    add logging config file

--- utils/constants.py
+++ utils/constants.py
@@ -2,3 +2,6 @@ COL_HEADLINE = "news_headline"
 COL_ARTICLE = "news_article"
 COL_CATEGORY = "news_category"
 COL_DATE = "date"
+
+ROOT_DIR = "D:/Dev/Project/nlp_news/"
+LOG_CONFIG_PATH = "D:/Dev/Project/nlp_news/utils/logging.conf"
