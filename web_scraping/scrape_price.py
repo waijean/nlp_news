78ef1e71570ec0ec81eb81d4ba842e6e24fe1d2d
Wai Jean Koh <kwj_97@hotmail.com>
2020-05-04 22:20:43 +0100

    add tests for scrape_price

--- web_scraping/scrape_price.py
+++ web_scraping/scrape_price.py
@@ -7,6 +7,7 @@ from utils.constants import LOG_CONFIG_PATH, DATA_PATH
 from utils.helper import get_df_info_to_logger
 from web_scraping.scrape_news import export_df
 from web_scraping.clean_price import drop_null_rows
+from web_scraping.web_url import price_url
 
 logging.config.fileConfig(fname=LOG_CONFIG_PATH, disable_existing_loggers=False)
 logger = logging.getLogger(__name__)
@@ -39,8 +40,7 @@ def get_df_from_url(url: str) -> pd.DataFrame:
 
 
 if __name__ == "__main__":
-    url = "https://query1.finance.yahoo.com/v7/finance/download/VEVE.L?period1=1412035200&period2=1588291200&interval=1d&events=history"
-    price_df = get_df_from_url(url)
+    price_df = get_df_from_url(price_url)
     processed_price_df = drop_null_rows(price_df)
     export_df(
         processed_price_df,
---
+++ web_scraping/web_url.py
@@ -0,0 +1,9 @@
+# yahoo finance link which gets the csv of VEVE for the entire history
+price_url = "https://query1.finance.yahoo.com/v7/finance/download/VEVE.L?period1=1412035200&period2=1588291200&interval=1d&events=history"
+
+# news_url from inshorts
+news_urls = [
+    "https://inshorts.com/en/read/politics",
+    "https://inshorts.com/en/read/business",
+    "https://inshorts.com/en/read/world",
+]
