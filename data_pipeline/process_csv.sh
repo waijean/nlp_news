58ac241d2c95816d0ca38e26b69539f0b4265909
Wai Jean Koh <kwj_97@hotmail.com>
2020-05-08 23:48:32 +0100

    add inserting header row

--- data_pipeline/process_csv.sh
+++ data_pipeline/process_csv.sh
@@ -5,14 +5,30 @@ CSV_PARTITION_DIR=$DATA_DIR/csv_partition
 ORIGINAL_CSV_FILE=$DATA_DIR/all-the-news-2-1.csv
 OUTPUT_CSV_FILE=$DATA_DIR/processed-all-the-news-2-1.csv
 PYTHON_FILE=/d/dev/Project/nlp_news/data_pipeline/partition_news.py
-
-#echo "Splitting large csv file into smaller csv files"
-#echo "Size of the large csv file: $(du -h $ORIGINAL_CSV_FILE | cut -f1)"
-#cd CSV_PARTITION_DIR
-#split -C 1G -d ORIGINAL_CSV_FILE news_csv_ && echo "Large csv file has been splitted to $CSV_PARTITION_DIR"
-#echo -e "Size of the smaller csv files: \n$(du -h -a $CSV_PARTITION_DIR)"
+PREFIX=news_csv_
 
 echo "Removing blank lines in large csv file"
 echo "Number of lines before removing blank lines: $(wc -l $ORIGINAL_CSV_FILE | cut -f1 -d' ')"
 sed '/^$/d' $ORIGINAL_CSV_FILE > $OUTPUT_CSV_FILE && echo "Processed csv file without blank lines: $OUTPUT_CSV_FILE"
 echo "Number of lines after removing blank lines $(wc -l $OUTPUT_CSV_FILE | cut -f1 -d' ')"
+
+echo "Splitting large csv file into smaller csv files"
+echo "Size of the large csv file: $(du -h $OUTPUT_CSV_FILE | cut -f1)"
+cd $CSV_PARTITION_DIR
+split -C 1G -d $ORIGINAL_CSV_FILE $PREFIX && echo "Large csv file has been splitted to $CSV_PARTITION_DIR"
+echo -e "Size of the smaller csv files: \n$(du -h -a $CSV_PARTITION_DIR)"
+
+echo "Inserting header row to each smaller csv file except for the first file"
+cd $CSV_PARTITION_DIR
+for file in $PREFIX*; do
+  echo $file
+  if [ $file = "news_csv_00" ]; then
+      FIRST_LINE=$(sed '1q;d' $file)
+      echo "Skipping $file"
+      continue;
+  fi
+  sed '1q;d' $file
+  sed -i "1 i $FIRST_LINE" $file
+  sed '1q;d' $file
+done && echo "Header row has been inserted"
+
